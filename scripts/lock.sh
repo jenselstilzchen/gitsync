4a9eca43feb709aca3ac17026f600a468850de02
jenselstilzchen <63961016+jenselstilzchen@users.noreply.github.com>
2021-09-05 19:34:18 +0200

    Create lock.sh

---
+++ scripts/lock.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+BLANK='#1d1f21'
+CLEAR='#c5c8c6'
+DEFAULT='#c5c8c6'
+TEXT='#1d1f21'
+WRONG='#a54242'
+VERIFYING='#5f819d'
+
+i3lock \
+--insidever-color=$CLEAR     \
+--ringver-color=$VERIFYING   \
+\
+--insidewrong-color=$CLEAR   \
+--ringwrong-color=$WRONG     \
+\
+--inside-color=$BLANK        \
+--ring-color=$DEFAULT        \
+--line-color=$BLANK          \
+--separator-color=$DEFAULT   \
+\
+--verif-color=$TEXT          \
+--wrong-color=$TEXT          \
+--time-color=$TEXT           \
+--date-color=$TEXT           \
+--layout-color=$TEXT         \
+--keyhl-color=$WRONG         \
+--bshl-color=$WRONG          \
+\
+--screen 1                   \
+--blur 5                     \
+--clock                      \
+--indicator                  \
+--time-str="%H:%M:%S"        \
+--date-str="%A, %m %Y"       \
+--keylayout 1                \
